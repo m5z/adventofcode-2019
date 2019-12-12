e91c13e791c3601ea7d85695d2562ef1991a99e0
m5z <m5z@example.com>
2019-12-12 22:57:05 +0100

    Day 10 Part 2

--- day10/day10.py
+++ day10/day10.py
@@ -1,8 +1,13 @@
+from fractions import Fraction
+
+
 def main():
     with open('day10_input.txt') as f:
         m = [list(line.rstrip()) for line in f.readlines()]
 
     part1(m)
+
+    part2(m, 25, 22)
 
 
 def part1(m):
@@ -16,6 +21,58 @@ def part1(m):
                     max_count = c
                     pos = (i, j)
     print(max_count, pos)
+
+
+def part2(m, pos_i, pos_j):
+    top = []
+    right = []
+    bot = []
+    left = []
+    asteroids = 0
+    for i in range(len(m)):
+        for j in range(len(m[i])):
+            if not (pos_i == i and pos_j == j) and m[i][j] == '#':
+                asteroids += 1
+                ri, rj = i - pos_i, j - pos_j
+                if rj == 0:
+                    t = (0, abs(ri), (i, j))
+                    if ri < 0:
+                        top.append(t)
+                    else:
+                        bot.append(t)
+                else:
+                    t = (Fraction(ri, rj), abs(ri), abs(rj), (i, j))
+                    if rj > 0:
+                        right.append(t)
+                    else:
+                        left.append(t)
+
+    top = sorted(top)
+    right = sorted(right)
+    bot = sorted(bot)
+    left = sorted(left)
+
+    destroyed = []
+    i = 0
+    while i < asteroids:
+        i = destroy(top, i, destroyed)
+        i = destroy(right, i, destroyed)
+        i = destroy(bot, i, destroyed)
+        i = destroy(left, i, destroyed)
+
+    result = destroyed[199]
+    print(result[1] * 100 + result[0])
+
+
+def destroy(queue, i, destroyed):
+    prev = None
+    for j in range(len(queue)):
+        if queue[j] is not None and queue[j][0] != prev:
+            destroyed.append(queue[j][-1])
+            prev = queue[j][0]
+            queue[j] = None
+            i += 1
+    return i
 
 
 def count(m, i, j):
